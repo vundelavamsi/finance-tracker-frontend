f2547c615108384e1fa5664793bb9b5e66fdff4f
Vamsi Vundela <vamsi.vundela@jumbotail.com>
2026-01-31 15:51:15 +0530

    change backend endpoint

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,8 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        // target: 'http://localhost:8000',
+        target: 'https://finance-tracker-backend-2n5w.onrender.com',
         changeOrigin: true,
       },
     },
